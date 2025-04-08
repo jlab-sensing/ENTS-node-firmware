ca250a8f0d9148c5ea1916dcebf12fdff9c9dd5b
Caden Jacobs <cadenjacobs@ucsc-guest-169-233-114-184.ucsc.edu>
2025-04-08 14:11:27 -0700

    Added start files to implement Capacitive Soil Moisture sensor

---
+++ stm32/lib/capSoil/include/capSoil.h
@@ -0,0 +1,71 @@
+/**
+ ******************************************************************************
+ * @file     capSoil.h
+ * @author   Caden Jacobs
+ * @brief    This file contains all the function prototypes for
+ *           the capSoiil.c file.
+ *
+ *           This library is designed to read measurements from a Capacitve Soil Moisture Sensor
+ *           [link]
+ * @date     4/8/2025
+ ******************************************************************************
+ */
+
+ #ifndef LIB_CAPSOIL_INCLUDE_CAPSOIL_H_
+ #define LIB_CAPSOIL_INCLUDE_CAPSOIL_H_
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ #include "ads.h"
+ 
+ typedef struct {
+   double capSoil_raw;
+   double capSoil_calibrated;
+ } phytos_measurments;
+ 
+ /**
+ ******************************************************************************
+ * @brief    Wrapper function for the ADC initilization.
+ *
+ * @param    void
+ * @return   HAL_StatusTypeDef
+ ******************************************************************************
+ */
+ HAL_StatusTypeDef CapSoilInit(void);
+ 
+ /**
+ ******************************************************************************
+ * @brief    Returns both the raw voltage value and a calibrated measurment
+ *           from a CapSoil sensor.
+ *
+ * @param    void
+ * @return   phytos_measurments
+ ******************************************************************************
+ */
+ phytos_measurments CapSoilGetMeasurment(void);
+ 
+ /**
+  * @brief Read CapSoil sensor and serialize measurement
+  *
+  * The voltage output of the CapSoil is measured. A calibration is applied
+  * to convert voltage into a leaf wetness measurement.
+  *
+  * Current voltage and leaf wetness are the same value, until a calibration
+  * is obtained.
+  *
+  * @note Implemented for the sensors library
+  *
+  * @see SensorsPrototypeMeasure
+  */
+ size_t CapSoil_measure(uint8_t *data);
+ 
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif  // LIB_CAPSOIL_INCLUDE_CAPSOIL_H_
