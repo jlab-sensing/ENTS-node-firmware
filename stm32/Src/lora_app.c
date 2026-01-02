c32ef798f61085996b1c56bd5aa230904dcd2bee
John Madden <jmadden173@pm.me>
2026-01-02 15:32:19 -0800

    Updated LoRa upload to batch sensor measurements

--- stm32/Src/lora_app.c
+++ stm32/Src/lora_app.c
@@ -44,6 +44,9 @@
 #include "status_led.h"
 #include "user_config.h"
 
+#include "lora_size.h"
+#include "sensor.h"
+
 #include <time.h>
 /* USER CODE END Includes */
 
@@ -489,13 +492,77 @@ static void SendTxData(void)
   uint8_t battery_level = GetBatteryLevel();
   uint16_t temperature = SYS_GetTemperatureLevel();
 
-  FramStatus status = FramGet(AppData.Buffer, &AppData.BufferSize);
-  if (status != FRAM_OK)
-  {
-    APP_LOG(TS_OFF, VLEVEL_M,
-            "Error getting data from fram buffer. FramStatus = %d", status);
-    return;
+
+
+  // array of measurements that will get uploaded
+  SensorMeasurement meas[16] = {};
+  size_t meas_count = 0;
+
+  // storing current payload size
+  size_t current_payload_size = 0;
+
+  // Get max LoRaWAN payload size
+  LoRaMacRegion_t region = 0;
+  LmHandlerGetActiveRegion(&region);
+  int8_t dr = 0;
+  LmHandlerGetTxDatarate(&dr);
+  uint8_t max_payload_size = lorawan_max_payload(region, dr);
+
+  FramStatus fram_status = FRAM_OK;
+  SensorStatus sensor_status = SENSOR_OK;
+
+
+  // NOTE: This is a temp fix for the function input. Ideally the metadata
+  // would be automatically handled and optimized for packet size.
+  Metadata meta = Metadata_init_default;
+
+  // Loop until the payload size is exceeded
+  while (current_payload_size <= max_payload_size) {
+    // buffer to store encoded measurements
+    uint8_t meas_buffer[256] = {};
+    uint8_t meas_buffer_size = 0;
+
+    // get next serialized measurement
+    fram_status = FramPeek(meas_count, meas_buffer, &meas_buffer_size);
+    if (fram_status != FRAM_OK) {
+      APP_LOG(TS_OFF, VLEVEL_M,
+              "Error peeking data from fram buffer. FramStatus = %d", fram_status);
+      break;
+    }
+
+    // decode measurement
+    sensor_status = DecodeSensorMeasurement(meas_buffer, meas_buffer_size, &meas[meas_count++]);
+    if (sensor_status != SENSOR_OK) {
+      APP_LOG(TS_OFF, VLEVEL_M,
+              "Error decoding sensor measurement from buffer. SensorStatus = %d", sensor_status);
+      break;
+    }
+
+    current_payload_size = RepeatedSensorMeasurementsSize(meta, meas,
+        meas_count, &current_payload_size); 
   }
+
+  // when exit we are over the payload limit so back off one
+  meas_count--;
+
+  // encode measurements into AppData buffer
+  EncodeRepeatedSensorMeasurements(meta, meas,
+      meas_count, AppData.Buffer, &AppData.BufferSize);
+
+  // drop uploaded measurements
+  for (int i = 0; i < meas_count; i++) {
+    FramDrop();
+  }
+
+
+  // Old code for measurements
+  //FramStatus status = FramGet(AppData.Buffer, &AppData.BufferSize);
+  //if (status != FRAM_OK)
+  //{
+  //  APP_LOG(TS_OFF, VLEVEL_M,
+  //          "Error getting data from fram buffer. FramStatus = %d", status);
+  //  return;
+  //}
 
   // NOTE: Old code from Steve packet compression
   //size_t size = 0;
@@ -522,7 +589,7 @@ static void SendTxData(void)
   APP_LOG(TS_OFF, VLEVEL_M, "\r\n");
   APP_LOG(TS_ON, VLEVEL_M, "%d\r\n", AppData.BufferSize);
 
-  AppData.Port = LORAWAN_SPS_MEAS_PORT;
+  AppData.Port = LORAWAN_SPS_MEAS_GENERIC_PORT;
 
   LmHandlerErrorStatus_t lmstatus;
   lmstatus = LmHandlerSend(&AppData, LORAWAN_DEFAULT_CONFIRMED_MSG_STATE, false);
@@ -533,7 +600,7 @@ static void SendTxData(void)
   else
   {
     APP_LOG(TS_OFF, VLEVEL_M, "Could not send request\r\n");
-    APP_LOG(TS_OFF, VLEVEL_M, "LmHandlerSend status = %d\r\n", status);
+    APP_LOG(TS_OFF, VLEVEL_M, "LmHandlerSend status = %d\r\n", lmstatus);
   }
 
   StatusLedOff();
--- stm32/lib/storage/src/fifo.c
+++ stm32/lib/storage/src/fifo.c
@@ -153,6 +153,67 @@ FramStatus FramGet(uint8_t *data, uint8_t *len) {
   return FRAM_OK;
 }
 
+FramStatus FramPeek(size_t idx, uint8_t *data, uint8_t *len) {
+  // Check if buffer is empty
+  if (buffer_len == 0) {
+    return FRAM_BUFFER_EMPTY;
+  }
+  if (idx >= buffer_len) {
+    return FRAM_OUT_OF_RANGE;
+  }
+
+  FramStatus status = FRAM_OK;
+  uint16_t temp_read_addr = read_addr;
+
+  // advance to idx
+  for (size_t i = 0; i < idx; i++) {
+    uint8_t temp_len;
+    status = FramRead(temp_read_addr, 1, &temp_len);
+    if (status != FRAM_OK) {
+      return status;
+    }
+    update_addr(&temp_read_addr, 1 + temp_len);
+  }
+
+  // read length
+  status = FramRead(temp_read_addr, 1, len);
+  if (status != FRAM_OK) {
+    return status;
+  }
+  update_addr(&temp_read_addr, 1);
+
+  // read data
+  status = FramRead(temp_read_addr, *len, data);
+  if (status != FRAM_OK) {
+    return status;
+  }
+
+  return FRAM_OK;
+}
+
+FramStatus FramDrop(void) {
+  // Check if buffer is empty
+  if (buffer_len == 0) {
+    return FRAM_BUFFER_EMPTY;
+  }
+
+  FramStatus status;
+  uint8_t len;
+
+  // read length
+  status = FramRead(read_addr, 1, &len);
+  if (status != FRAM_OK) {
+    return status;
+  }
+  update_addr(&read_addr, 1 + len);
+
+  // Decrement buffer length
+  --buffer_len;
+
+  FramSaveBufferState(read_addr, write_addr, buffer_len);
+  return FRAM_OK;
+}
+
 uint16_t FramBufferLen(void) { return buffer_len; }
 
 FramStatus FramBufferClear(void) {
