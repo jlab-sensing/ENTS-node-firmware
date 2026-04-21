40b7950c285e2446f75967527ea109a89aa4d4de
Jack Lin <jlin143@ucsc.edu>
2026-04-21 16:06:15 -0700

    example_tca9535

--- stm32/lib/gpio_expander/include/tca9535.h
+++ stm32/lib/gpio_expander/include/tca9535.h
@@ -28,6 +28,8 @@ extern "C" {
 /************************** I2C Address ***************************************/
 #define TCA9535_ADDRESS (0x20 << 1)  // I2C 7-bit Address [0100(A2)(A1)(A0)] + (R/~W)
 
+#define TCA9535_MEM_ADDRESS_SIZE 1 // 1 byte memory address
+
 /************************** I2C Registers *************************************/
 #define TCA9535_INPUT_REG0 0x00  // Input status register
 #define TCA9535_INPUT_REG1 0x01  // Input status register
@@ -234,15 +236,15 @@ extern TCA9535Regs TCA9535_Reg_map;
 // High level functions
 bool TCA9535Init(bool interruptEnable);
 
-void Tca9535WritePin(uint8_t IO_Port, uint8_t IO_Pin, GPIO_PinState PinState);
-void Tca9535TogglePin(uint8_t IO_Port, uint8_t IO_Pin);
-GPIO_PinState Tca9535ReadPin(uint8_t IO_Port, uint8_t IO_Pin);
-void Tca9535SetDirection(uint8_t IO_Port, uint8_t IO_Pin, uint8_t Direction);
+void TCA9535WritePin(uint8_t IO_Port, uint8_t IO_Pin, GPIO_PinState PinState);
+void TCA9535TogglePin(uint8_t IO_Port, uint8_t IO_Pin);
+GPIO_PinState TCA9535ReadPin(uint8_t IO_Port, uint8_t IO_Pin);
+void TCA9535SetDirection(uint8_t IO_Port, uint8_t IO_Pin, uint8_t Direction);
 
 // Low level functions
 void TCA9535InitStructDefault(TCA9535Regs* Regs);
-void Tca9535InterruptInit();
-void Tca9535InterruptDeinit();
+void TCA9535InterruptInit();
+void TCA9535InterruptDeinit();
 
 HAL_StatusTypeDef TCA9535WriteOutput(TCA9535Regs* Regs);
 HAL_StatusTypeDef TCA9535WritePolarity(TCA9535Regs* Regs);
@@ -256,6 +258,8 @@ HAL_StatusTypeDef TCA9535ReadPolarity(TCA9535Regs* Regs);
 HAL_StatusTypeDef TCA9535ReadConfig(TCA9535Regs* Regs);
 
 HAL_StatusTypeDef TCA9535ReadAll(TCA9535Regs* Regs);
+
+void TCA9535PrintAll(TCA9535Regs* Regs);
 
 /**
  * @}
