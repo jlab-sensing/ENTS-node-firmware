515af429e5b0571c6bcd773ba1aee4d45afa4719
kots727 <90489449+kots727@users.noreply.github.com>
2026-07-20 12:38:40 -0700

    change to strtol

--- esp32/lib/user_config/src/config_server.cpp
+++ esp32/lib/user_config/src/config_server.cpp
@@ -215,7 +215,7 @@ void handleSave() {
           }
         } else {
           config.enabled_sensors_multiple[config.enabled_sensors_multiple_count]
-              .index = selected_sensor_index.toInt();
+          .index = strtol(selected_sensor_index.c_str(), NULL, 0);
         }
         break;
       case EnabledSensor_D10:
