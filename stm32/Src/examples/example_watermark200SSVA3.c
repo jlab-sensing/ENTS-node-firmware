a519025566eda42920d7de754440d9f1e894c2d7
Jack Lin <jlin143@ucsc.edu>
2026-08-20 20:25:53 -0700

    example watermark updated

--- stm32/Src/examples/example_watermark200SSVA3.c
+++ stm32/Src/examples/example_watermark200SSVA3.c
@@ -49,7 +49,24 @@ int main(void) {
   //  6        PA10          3   I2C1 SDA (NOT RECOMMENDED FOR ADC)
   // 11        PA15         18
 
-  Watermark200SSVA3_Init();
+  UserConfiguration cfg = UserConfiguration_init_zero;
+  cfg.enabled_sensors_multiple_count = 4; // not used in this example
+  cfg.enabled_sensors_multiple[0].cell_id = 200; // not used in this example
+  cfg.enabled_sensors_multiple[0].enabled_sensor = EnabledSensor_WATERMARK200SS;
+  cfg.enabled_sensors_multiple[0].index = 21;  // ADC channel 1, pin 21
+  cfg.enabled_sensors_multiple[1].cell_id = 201; // not used in this example
+  cfg.enabled_sensors_multiple[1].enabled_sensor = EnabledSensor_WATERMARK200SS;
+  cfg.enabled_sensors_multiple[1].index = 22;  // ADC channel 2, pin 22
+  cfg.enabled_sensors_multiple[2].cell_id = 202; // not used in this example
+  cfg.enabled_sensors_multiple[2].enabled_sensor = EnabledSensor_WATERMARK200SS;
+  cfg.enabled_sensors_multiple[2].index = 24;  // ADC channel 3, pin 24
+  cfg.enabled_sensors_multiple[3].cell_id = 203; // not used in this example
+  cfg.enabled_sensors_multiple[3].enabled_sensor = EnabledSensor_WATERMARK200TS;
+  cfg.enabled_sensors_multiple[3].index = 18;  // ADC channel 11, pin 18
+  Watermark200Init(&(cfg.enabled_sensors_multiple[0]));
+  Watermark200Init(&(cfg.enabled_sensors_multiple[1]));
+  Watermark200Init(&(cfg.enabled_sensors_multiple[2]));
+  Watermark200Init(&(cfg.enabled_sensors_multiple[3]));
 
   // init systemapp
   SystemApp_Init();
@@ -60,15 +77,14 @@ int main(void) {
   uint32_t conversion_count = 0;
 
   while (1) {
-    Watermark200SSVA3_GetMeasurement();
     APP_PRINTF("%d Soil Tension (WM1): %lf kPa\r\n", conversion_count,
-               Watermark200SSVA3_GetWM1());
+               Watermark200SS_GetMeasurement(&(cfg.enabled_sensors_multiple[0])));
     APP_PRINTF("%d Soil Tension (WM2): %lf kPa\r\n", conversion_count,
-               Watermark200SSVA3_GetWM2());
+               Watermark200SS_GetMeasurement(&(cfg.enabled_sensors_multiple[1])));
     APP_PRINTF("%d Soil Tension (WM3): %lf kPa\r\n", conversion_count,
-               Watermark200SSVA3_GetWM3());
+               Watermark200SS_GetMeasurement(&(cfg.enabled_sensors_multiple[2])));
     APP_PRINTF("%d Soil Temperature:   %lf C\r\n", conversion_count,
-               Watermark200SSVA3_GetWMTemp());
+               Watermark200TS_GetMeasurement(&(cfg.enabled_sensors_multiple[3])));
 
     APP_PRINTF("\r\n");
 
