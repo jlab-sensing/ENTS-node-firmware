66a51562d2d2b70832fa355be0685394991dfabf
Jack Lin <jlin143@ucsc.edu>
2026-09-17 16:38:58 -0700

    solenoid update wip

--- stm32/lib/solenoid/include/solenoid.h
+++ stm32/lib/solenoid/include/solenoid.h
@@ -5,9 +5,9 @@
  * @brief    This file contains all the function prototypes for
  *           the solenoid.c file.
  *
- *           This library is designed to integrate a solenoid used for
- *irrigation.
- *           https://www.orbitonline.com/products/solenoids?variant=45109892612264&country=US&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&utm_source=paid+search&utm_medium=google+ads&utm_campaign=core+shopping&gad_source=1&gad_campaignid=19652308266&gbraid=0AAAAACw56Spsna6BxGUmTXQEtBiAdD4xG&gclid=CjwKCAjw-svEBhB6EiwAEzSdrsAHw1dSYI86-bRo6kxv42twG7OPn3huLm-9Bx99XAEWvaCfzjyhmxoCmnAQAvD_BwE
+ *           This library is designed to control an irrigation solenoid
+ *           connected to a relay, but can also be used to control other
+ *           relay-connected devices.
  * @date     8/6/2025
  ******************************************************************************
  */
@@ -22,55 +22,74 @@ extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "ads.h"
+#ifndef SOLENOID_LATCH_DELAY_MS
+// Increase the latch time if the solenoid requires additional time to latch.
+#define SOLENOID_LATCH_DELAY_MS 500
+#endif
 
-#define SOLENOID_MAX 5
-
-/** States of the solenoid */
 typedef enum {
-  SOLENOID_OFF,
-  SOLENOID_ON,
-} StatusSolenoid;
+  SOLENOID_STATE_OFF = 0,
+  SOLENOID_STATE_ON = 1,
+} SolenoidState;
 
+/*
+ Note: The Normally Open (NO) Normally Closed (NC) notation on the SolenoidType
+ struct describes circuit energization.
+
+ - A hold-type solenoid that is NO means that it is typically OFF (flow
+ blocked), continuously energizing the relay turns it ON (flow permitted).
+ - A hold-type solenoid that is NC means that it is typically ON (flow
+ permitted), continuously energizing the relay turns it OFF (flow blocked).
+ - We always assume that latching solenoids are wired in the NO position. The
+ starting state is typically OFF. A pulse will toggle between ON and OFF. A
+ latching solenoid retains its state even when deenergized.
+*/
+typedef enum {
+  // standard solenoid, continuously energize to close-circuit/activate.
+  SOLENOID_TYPE_HOLD_NO = 0,
+  // standard solenoid, continuously energize to open-circuit/deactivate.
+  // This option is not typically used.
+  SOLENOID_TYPE_HOLD_NC = 1,
+  // latching solenoid, pulse to toggle latch. Starts open/deactivated.
+  SOLENOID_TYPE_LATCH_NO,
+} SolenoidType;
+
+// state reflects the current state of the solenoid, and should only be modifed
+// by the Solenoid* functions (and for initialization).
 typedef struct {
-  uint8_t pin;
-  StatusSolenoid state;
-} SolenoidParameter;
+  uint32_t pin;  // physical pin # on the 40-pin header.
+  SolenoidType type;
+  SolenoidState state;  // current state
+} Solenoid;
 
 /**
  ******************************************************************************
  * @brief    Wrapper function for the Solenoid initilization.
+ *           solenoid->state is reset to the resting position based on type.
+ *           i.e. HOLD_NO=OFF, HOLD_NC=ON, LATCH_NO=OFF
  *
- * @param    void
- * @return   HAL_StatusTypeDef
+ * @note     Do not reinitialize a latching solenoid while it is activated.
+ *           This will result in the software state being the inverse of the
+ *           physical state of the latching solenoid.
+ * 
+ * @pre      TCA9535Init() if using GPIO expander pins for solenoid control.
+ *
+ * @param    solenoid
+ * @return   void
  ******************************************************************************
  */
-void SolenoidInit(EnabledSensorMultiple* sensor);
+void SolenoidInit(Solenoid* solenoid);
 
 /**
  ******************************************************************************
- * @brief    Returns both the raw voltage value and a calibrated measurement
- *           from a water Flow sensor.
+ * @brief    Changes the state of the solenoid.
  *
- * @param    void
- * @return   measurements
+ * @param    solenoid
+ * @param    state_to_apply
+ * @return   void
  ******************************************************************************
  */
-void SolenoidOpen(uint8_t pinNumber);
-
-/**
- * @brief Read water Flow sensor and serialize measurement
- *
- * The voltage output of the water Flow is measured. A calibration is applied
- * to convert voltage into a leaf wetness measurement.
- *
- * Current voltage and Flow are the same value, until a calibration
- * is obtained.
- *
- *
- * @see SensorsPrototypeMeasure
- */
-void SolenoidClose(uint8_t pinNumber);
+void SolenoidChangeState(Solenoid* solenoid, SolenoidState state_to_apply);
 
 #ifdef __cplusplus
 }
--- stm32/lib/solenoid/src/solenoid.c
+++ stm32/lib/solenoid/src/solenoid.c
@@ -3,9 +3,9 @@
  * @file    solenoid.c
  * @author  Caden Jacobs
  *
- * @brief   This library is designed to communicate with a solenoid to open and
- *          close it based on GPIO pins
- *          https://www.orbitonline.com/products/solenoids?variant=45109892612264&country=US&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&utm_source=paid+search&utm_medium=google+ads&utm_campaign=core+shopping&gad_source=1&gad_campaignid=19652308266&gbraid=0AAAAACw56Spsna6BxGUmTXQEtBiAdD4xG&gclid=CjwKCAjw-svEBhB6EiwAEzSdrsAHw1dSYI86-bRo6kxv42twG7OPn3huLm-9Bx99XAEWvaCfzjyhmxoCmnAQAvD_BwE
+ * @brief   This library is designed to control an irrigation solenoid
+ *          connected to a relay, but can also be used to control other
+ *          relay-connected devices.
  * @date    8/6/2025
  ******************************************************************************
  */
@@ -14,130 +14,270 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "stm32_seq.h"
-#include "stm32wlxx_hal_def.h"
-#include "utilities_def.h"
+#include "tca9535.h"
 
-static StatusSolenoid Solenoid;
-static uint8_t activeSolenoids = 0;
-SolenoidParameter *solenoid[SOLENOID_MAX];
+// static StatusSolenoid Solenoid;
+// static uint8_t activeSolenoids = 0;
+// Solenoid *solenoid[SOLENOID_MAX];
 
-void SolenoidInit(SolenoidParameter *solenoid) {
-  __HAL_RCC_GPIOA_CLK_ENABLE();  // Enable GPIOA clock
-  __HAL_RCC_GPIOB_CLK_ENABLE();  // Enable GPIOB clock
-
-  // Use Pin PA10 to toggle solenoid
+void SolenoidInit(Solenoid *solenoid) {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if (solenoid->pin) {
-    if (solenoid->pin == 5) {
-      GPIO_InitStruct.Pin = GPIO_PIN_9;
-      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);  // LOW = Relay OFF
-    }
-    if (solenoid->pin == 3) {
-      // original solenoid pin used
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+  if (solenoid->type == SOLENOID_TYPE_HOLD_NO) {
+    solenoid->state = SOLENOID_STATE_OFF;
+  } else if (solenoid->type == SOLENOID_TYPE_HOLD_NC) {
+    solenoid->state = SOLENOID_STATE_ON;
+  } else if (solenoid->type == SOLENOID_TYPE_LATCH_NO) {
+    solenoid->state = SOLENOID_STATE_OFF;
+  }
+
+  switch (solenoid->pin) {
+    case 3:
+      __HAL_RCC_GPIOA_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
       GPIO_InitStruct.Pin = GPIO_PIN_10;
       HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);  // LOW = Relay OFF
-    }
-    if (solenoid->pin == 15) {
+      break;
+    case 5:
+      __HAL_RCC_GPIOA_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_9;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+      break;
+    case 8:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_6;
+      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+      break;
+    case 10:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_7;
+      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+      break;
+    case 15:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
       GPIO_InitStruct.Pin = GPIO_PIN_9;
       HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);  // LOW = Relay OFF
-    }
-    if (solenoid->pin == 23) {
-      GPIO_InitStruct.Pin = GPIO_PIN_10;
+      break;
+    case 16:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_13;
       HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);  // LOW = Relay OFF
-    }
-    if (solenoid->pin == 19) {
+      break;
+    case 18:
+      __HAL_RCC_GPIOA_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_15;
+      HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+      break;
+    case 19:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);
       GPIO_InitStruct.Pin = GPIO_PIN_15;
       HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);  // LOW = Relay OFF
+      break;
+    case 21:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_14;
+      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+      break;
+    case 22:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_3;
+      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+      break;
+    case 23:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_10;
+      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+      break;
+    case 24:
+      __HAL_RCC_GPIOB_CLK_ENABLE();
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+      GPIO_InitStruct.Pin = GPIO_PIN_4;
+      HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+      break;
+    case 27:  // P10
+      TCA9535WritePin(1, 0, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 0, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 29:  // P11
+      TCA9535WritePin(1, 1, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 1, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 31:  // P12
+      TCA9535WritePin(1, 2, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 2, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 33:  // P13
+      TCA9535WritePin(1, 3, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 3, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 35:  // P14
+      TCA9535WritePin(1, 4, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 4, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 37:  // P15
+      TCA9535WritePin(1, 5, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 5, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 28:  // P16
+      TCA9535WritePin(1, 6, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 6, TCA9535_CONFIG_OUTPUT);
+      break;
+    case 36:  // P17
+      TCA9535WritePin(1, 7, GPIO_PIN_RESET);
+      TCA9535SetDirection(1, 7, TCA9535_CONFIG_OUTPUT);
+      break;
+    default:
+      // invalid pin
+      break;
+  }
+}
+
+void SolenoidChangeState(Solenoid *solenoid, SolenoidState state_to_apply) {
+  GPIO_TypeDef *hal_port = GPIOA;
+  uint16_t hal_pin = GPIO_PIN_10;
+  uint8_t tca9535_port = 1;
+  uint8_t tca9535_pin = 0;
+
+  if (state_to_apply == solenoid->state) {
+    // Solenoid is already in the correct state.
+    return;
+  } else if (state_to_apply != SOLENOID_STATE_OFF &&
+             state_to_apply != SOLENOID_STATE_ON) {
+    // invalid state_to_apply, do not act.
+    return;
+  }
+
+  switch (solenoid->pin) {
+    case 3:
+      hal_port = GPIOA;
+      hal_pin = GPIO_PIN_10;
+      break;
+    case 5:
+      hal_port = GPIOA;
+      hal_pin = GPIO_PIN_9;
+      break;
+    case 8:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_6;
+      break;
+    case 10:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_7;
+      break;
+    case 15:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_9;
+      break;
+    case 16:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_13;
+      break;
+    case 18:
+      hal_port = GPIOA;
+      hal_pin = GPIO_PIN_15;
+      break;
+    case 19:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_15;
+      break;
+    case 21:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_14;
+      break;
+    case 22:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_3;
+      break;
+    case 23:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_10;
+      break;
+    case 24:
+      hal_port = GPIOB;
+      hal_pin = GPIO_PIN_4;
+      break;
+    case 27:  // P10
+      tca9535_port = 1;
+      tca9535_pin = 0;
+      break;
+    case 29:  // P11
+      tca9535_port = 1;
+      tca9535_pin = 1;
+      break;
+    case 31:  // P12
+      tca9535_port = 1;
+      tca9535_pin = 2;
+      break;
+    case 33:  // P13
+      tca9535_port = 1;
+      tca9535_pin = 3;
+      break;
+    case 35:  // P14
+      tca9535_port = 1;
+      tca9535_pin = 4;
+      break;
+    case 37:  // P15
+      tca9535_port = 1;
+      tca9535_pin = 5;
+      break;
+    case 28:  // P16
+      tca9535_port = 1;
+      tca9535_pin = 6;
+      break;
+    case 36:  // P17
+      tca9535_port = 1;
+      tca9535_pin = 7;
+      break;
+    default:
+      // invalid pin
+      return;
+      break;
+  }
+
+  if (solenoid->pin < 27) {
+    // mcu
+    if (solenoid->type == SOLENOID_TYPE_LATCH_NO) {
+      HAL_GPIO_WritePin(hal_port, hal_pin, GPIO_PIN_SET);
+      HAL_Delay(SOLENOID_LATCH_DELAY_MS);
+      HAL_GPIO_WritePin(hal_port, hal_pin, GPIO_PIN_RESET);
+    } else {
+      /*
+      hold-type vs state_to_apply
+      |   +   | NO=0 | NC=1 |
+      -------------------------
+      | OFF=0 |  0   |  1   |
+      | ON =1 |  1   |  2 (% 2 = 0) |
+      GPIO_PIN_RESET = 0
+      GPIO_PIN_SET = 1
+      */
+      HAL_GPIO_WritePin(hal_port, hal_pin,
+                        (solenoid->type + state_to_apply) % 2);
     }
-
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;  // Push-pull output
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-    // Init state of Solenoid
-    solenoid->state = SOLENOID_OFF;
+  } else {
+    // tca9535
+    if (solenoid->type == SOLENOID_TYPE_LATCH_NO) {
+      TCA9535WritePin(tca9535_port, tca9535_pin, GPIO_PIN_SET);
+      HAL_Delay(SOLENOID_LATCH_DELAY_MS);
+      TCA9535WritePin(tca9535_port, tca9535_pin, GPIO_PIN_RESET);
+    } else {
+      TCA9535WritePin(tca9535_port, tca9535_pin,
+                      (solenoid->type + state_to_apply) % 2);
+    }
   }
+
+  solenoid->state = state_to_apply;
 }
-
-void SolenoidOpen(SolenoidParameter *solenoid) {
-  printf("SOLENOID_OPEN: Setting %i to HIGH (Relay ON)\n", solenoid->pin);
-  switch (solenoid->pin) {
-    case 5:
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);  // HIGH = Relay ON
-      solenoid->state = SOLENOID_ON;
-      break;
-
-    case 3:
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);  // HIGH = Relay ON
-      solenoid->state = SOLENOID_ON;
-      break;
-
-    case 15:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);  // HIGH = Relay ON
-      solenoid->state = SOLENOID_ON;
-      break;
-
-    case 23:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);  // HIGH = Relay ON
-      solenoid->state = SOLENOID_ON;
-      break;
-
-    case 19:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);  // HIGH = Relay ON
-      solenoid->state = SOLENOID_ON;
-      break;
-  }
-}
-
-void SolenoidClose(SolenoidParameter *solenoid) {
-  printf("SOLENOID_OPEN: Setting %i to LOW (Relay ON)\n", solenoid->pin);
-  switch (solenoid->pin) {
-    case 5:
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);  // LOW = Relay OFF
-      solenoid->state = SOLENOID_OFF;
-      break;
-
-    case 3:
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);  // LOW = Relay OFF
-      solenoid->state = SOLENOID_OFF;
-      break;
-
-    case 15:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);  // LOW = Relay OFF
-      solenoid->state = SOLENOID_OFF;
-      break;
-
-    case 23:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);  // LOW = Relay OFF
-      solenoid->state = SOLENOID_OFF;
-      break;
-
-    case 19:
-      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_RESET);  // LOW = Relay OFF
-      solenoid->state = SOLENOID_OFF;
-      break;
-  }
-}
-
-/* Private funct*/
-// static void SolenoidUpdate(void);
-
-// void SolenoidUpdate(void)
-// {
-//   // TODO parse the files from downlink.c/h into here, for now it is just a
-//   simple toggle if(Solenoid == SOLENOID_OFF)
-//   {
-//     SolenoidOpen();
-//   } else if (Solenoid == SOLENOID_ON)
-//   {
-//     SolenoidClose();
-//   }
-// }
